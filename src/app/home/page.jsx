8f207bc10da5cf546bc1ceabc4415fc45a4f10e2
yrkeviin <kevineziquiellima160@gmail.com>
2025-09-23 11:54:15 -0300

    Feat: Header and colors

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -7,22 +7,29 @@ export default function Home() {
         <div className={styles.container}>
             <Header />
 
+            <div className={styles.separator}></div>
+
+            <div className={styles.description}>
+                <h1>Explore Our Wine Selection</h1>
+                <p>Discover a handpicked collection of South Tyrolean wines.</p>
+            </div>
+
             <div className={styles.menu}>
                 <div className={styles.card}>
-                    <h1>Card 1</h1>
-                    <p>texto1</p>
+                    <h1>Storia ed Identità</h1>
+                    <p>scopri</p>
                 </div>
                 <div className={styles.card2}>
-                    <h1>Card 2</h1>
-                    <p>texto2</p>
+                    <h1>Cantina e Vini</h1>
+                    <p>scopri</p>
                 </div>
                 <div className={styles.card3}>
-                    <h1>Card 3</h1>
-                    <p>texto3</p>
+                    <h1>Altri prodotti</h1>
+                    <p>scopri</p>
                 </div>
                 <div className={styles.card4}>
-                    <h1>Card 4</h1>
-                    <p>texto4</p>
+                    <h1>Wineshop ed Event</h1>
+                    <p>scopri</p>
                 </div>
             </div>
         </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,6 @@ import styles from "./Header.module.css";
 export default function Header() {
     return (
         <div className={styles.header}>
-            <div className={styles.title}>
-                <h1 className={styles.title}>Os Melhores Vinhos</h1>
-            </div>
-
             <div className={styles.nav}>
                 <ul>
                     <a href=""><li className={styles.link}>Home</li></a>
@@ -14,6 +10,11 @@ export default function Header() {
                     <a href=""><li className={styles.link}>Details</li></a>
                 </ul>
             </div>
+
+            <div className={styles.logo}>
+                <img src="/images/logov1-removebg-preview.png" alt="Os Melhores Vinhos" />
+            </div>
+
         </div>
     );
 }
